df2403c7e94ffca3fa2600330f29ddc71fc12678
Johansixx <70510042+Johansixx@users.noreply.github.com>
2020-09-21 11:58:39 +0200

    Create Biblotek.py

---
+++ Biblotek.py
@@ -0,0 +1,25 @@
+class Library:
+
+    class Book:
+
+           def __init__(self, titel, author, pages, price, year):
+        self.titel = titel
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.year = year
+
+        pass
+
+    class Movie:
+        pass
+
+    class CD:
+        pass
+
+    def __init__(self, titel, author, pages, price, year):
+        self.titel = titel
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.year = year
